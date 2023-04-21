e7e001ed20aef2932d61e19d4d437da98d7b50c3
向阳 <xiangqian_liu@163.com>
2023-04-21 18:00:12 +0800

    feat: controller

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,5 +1,15 @@
+import { PATH_METADATA } from './common'
+
 export function Controller(router: string) {
-  return function (target: any) {
-    console.log(router, target)
+  const defaultPath = '/'
+  return function (target: object) {
+    const path = router || defaultPath
+    Reflect.defineMetadata(PATH_METADATA, path, target)
+  }
+}
+
+export function Get(router: string) {
+  return function (target: object, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log(target.constructor, router, propertyKey, descriptor.value)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { Controller } from './decorators'
+import 'reflect-metadata'
+import { Controller, Get } from './decorators'
 
 @Controller('user')
 class UserController {
-
+  constructor() {}
+  @Get('info')
+  getUserInfo() {}
 }
+
+const user = new UserController()
+
+user.getUserInfo()
